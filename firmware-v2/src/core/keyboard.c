850c96fe7775a3c9583d71c83ed5888a2d324ea1
travmonkey <travis@batk.me>
2025-05-23 00:21:45 -0600

    Type changes

--- firmware-v2/src/core/keyboard.c
+++ firmware-v2/src/core/keyboard.c
@@ -5,57 +5,53 @@
 #include "keyboard.h"
 #include "class/hid/hid_device.h"
 #include "matrix.h"
+#include <keymap.h>
 #include <pico/types.h>
 #include <stdint.h>
 #include <string.h>
-#include <keymap.h>
-#include "layer_processor.h"
 #include "usb_descriptors.h"
+#include "layer_processor.h"
 
-void keyboard_init(keymap_modes_t *const modes, keymap_t *const keymap, mod_keys_t *const layer_and_mode) {
+void keyboard_init(keymap_t *const keymap,
+                   layer_keys_t *const layer_info) {
   // Initialize the keyboard
-  // Need layer and debouncing init too
-  initialize_keymap_modes_type(modes);
-  initialize_keymap_type(keymap, modes);
-  construct_keymaps(modes);
-  initialize_layers(layer_and_mode); // Initialize the layer and mode keys
-  matrix_init();
+  initialize_keymaps(keymap);
+  initialize_layers(layer_info);
+  store_layer_indices(keymap, layer_info);
+  matrix_init(); // Init gpio pins for reading and writing
 }
 
-void process_matrix(keycode_report_t *report, keymap_t *keymap, mod_keys_t *layer_and_mode) {
+void set_keycodes(keymap_t const *const keymap, keycode_report_t *const report, matrix_state_t const *const state) {
+  for (uint8_t index = 0; index < MAX_KEYCODES; index++) {
+    if (state->activated_keys[index] < (TOTAL_COLS * TOTAL_ROWS)) {
+      report->keycodes[index] = keymap->active_keymap[state->activated_keys[index]];
+    }
+  }
+}
+
+void process_matrix(keycode_report_t *report, keymap_t *keymap) {
   matrix_state_t state;
   matrix_clear(&state); // Clear the memory of the matrix state to be safe
-
-  // For safety; check for a null pointer
-  if (report == NULL) {
-    return;
-  }
 
   // Clear last report by setting memory to 0
   memset(report->keycodes, 0, MAX_KEYCODES);
   report->count = 0;
 
-  // Get a scan of the matrix
-  matrix_read(&state);
-  set_layer_and_mode(&state, keymap, layer_and_mode);
+  matrix_read(&state); // Scan matrix
+  matrix_convert(&state); // Extract array of indices from scan
+
+  set_layer(keymap, return_layer(&state, &keymap->layer_indices));
+  matrix_trim(&state);
+
+  set_keycodes(keymap, report, &state);
 
   // TODO, apply debouncing and layer handing
-
-  // Iterate through the matrix
-  for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-    for (uint8_t col = 0; col < MATRIX_COLS; col++) {
-      // Read byte stored in memory
-      if (state.state[row][col]) {
-        // Get keycode from keymap
-        uint8_t keycode = return_keycode(keymap, row, col);
-        if (keycode != 0 && report->count < MAX_KEYCODES) {
-          // Update keycode to index of count
-          report->keycodes[report->count] = keycode;
-          report->count++; // Increment count
-        }
-      }
-    }
-  }
+  // TODO, implement way to scan half a matrix for split keyboards
+  // I think of two ways to do this; make the structs for each half and do them
+  // independently OR, I was thinking to just define the pins left->right.
+  // Divide pins by two. Left half maxes at half Right half starts half way
+  // through. Gonna have to implement UART first Another IDEA: Create another
+  // variable in state, send state over UART, and concat them
 }
 
 void send_keyboard_report(keycode_report_t *const report) {
@@ -63,6 +59,11 @@ void send_keyboard_report(keycode_report_t *const report) {
     // Send the report to the host
     tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, report->keycodes);
   }
+  // TODO, implement modifier keys
+  // TODO, implement consumer keys
+  // TODO, check for different layered keys on the last report, and change
+  // the next key to be sent to match.
+  // Clear the keycodes for the next report
   memcpy(report->previous_keycodes, report->keycodes, MAX_KEYCODES);
-  memset(report->keycodes, 0, MAX_KEYCODES); // Clear the keycodes for the next report
+  memset(report->keycodes, 0, MAX_KEYCODES);
 }
--- firmware-v2/src/core/keyboard.h
+++ firmware-v2/src/core/keyboard.h
@@ -8,16 +8,19 @@
 #define MAX_KEYCODES 6
 
 typedef struct {
-  uint8_t keycodes[MAX_KEYCODES]; // Array to hold keycodes
+  uint8_t keycodes[MAX_KEYCODES];          // Array to hold keycodes
   uint8_t previous_keycodes[MAX_KEYCODES]; // Previous keycodes for debouncing
-  uint8_t modifiers[MAX_KEYCODES]; // Modifiers (e.g., Shift, Control)
-  uint8_t count; // Number of keycodes in the array
+  uint8_t modifiers[MAX_KEYCODES];         // Modifiers (e.g., Shift, Control)
+  uint8_t count;                           // Number of keycodes in the array
 } keycode_report_t;
 
-void keyboard_init(keymap_modes_t *const modes, keymap_t *const keymap, mod_keys_t *const layer_and_mode);
+void keyboard_init(keymap_t *const keymap,
+                   layer_keys_t *const layer_and_mode);
 
-void process_matrix(keycode_report_t *report, keymap_t *const keymap, mod_keys_t *const layer_and_mode);
+void process_matrix(keycode_report_t *report, keymap_t *keymap);
 
 void send_keyboard_report(keycode_report_t *report);
+
+void set_keycodes(keymap_t const *const keymap, keycode_report_t *const report, matrix_state_t const *const state);
 
 #endif // KEYBOARD_H
