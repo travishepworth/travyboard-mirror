c8e2eae1136355e32c6c7fa026f7b07bba2c5c17
travmonkey <t.coding@batk.me>
2025-06-08 12:40:27 -0600

    Better comments

--- firmware-v2/src/core/matrix/matrix.c
+++ firmware-v2/src/core/matrix/matrix.c
@@ -6,11 +6,13 @@
 matrix_read_t matrix_read;
 matrix_init_t matrix_init;
 
+// SELECTOR: Select which matrix functions to use
 void select_matrix_backend() {
   matrix_read = KEYBOARD_MODE_SPLIT ? matrix_read_split : matrix_read_single;
   matrix_init = KEYBOARD_MODE_SPLIT ? matrix_init_split : matrix_init_single;
 }
 
+// HELPER: Clear the matrix
 void matrix_clear(matrix_state_t *const state) {
   // Clear the matrix state
   memset(state->state, 0, sizeof(state->state));
@@ -18,12 +20,15 @@ void matrix_clear(matrix_state_t *const state) {
   state->total_activated_keys = 0;
 };
 
+// INIT: Init function
 void matrix_initialize_metadata(matrix_metadata_t *const metadata) {
   // Initialize the metadata for the matrix
   memset(metadata, 0, sizeof(*metadata));
 }
 
+// CORE: Extract indecies from bool array to single array
 void matrix_convert(matrix_state_t *const state) {
+  memset(state->activated_keys, 0, sizeof(state->activated_keys));
   for (uint8_t matrix_index = 0; matrix_index < (MATRIX_SIZE); matrix_index++) {
     if (state->state[matrix_index]) {
       state->activated_keys[state->total_activated_keys++] = matrix_index;
@@ -31,30 +36,24 @@ void matrix_convert(matrix_state_t *const state) {
   }
 }
 
-void matrix_trim(matrix_state_t *const state) {
-  if (state->total_activated_keys > 6) {
-    memset(&state->activated_keys[6], 0, (MATRIX_SIZE - 6) * sizeof(uint8_t)); // Might as well clear to the end
-    state->total_activated_keys = 6; // Max keys for key report
-  }
-}
-
+// HELPER: Check if matrix state is empty
 bool matrix_is_empty(matrix_state_t const *const state) {
-  // Check if the matrix state is empty
   for (uint8_t i = 0; i < MATRIX_SIZE; i++) {
     if (state->state[i]) {
-      return false; // Found an active key
+      return false;
     }
   }
-  return true; // No active keys found
+  return true;
 }
 
+// HELPER: Copy entire state to other state
 void matrix_copy(matrix_state_t *const target_matrix, matrix_state_t const source_matrix) {
-  // Copy the state from source_matrix to target_matrix
   memcpy(target_matrix->state, source_matrix.state, sizeof(target_matrix->state));
   memcpy(target_matrix->activated_keys, source_matrix.activated_keys, sizeof(target_matrix->activated_keys));
   target_matrix->total_activated_keys = source_matrix.total_activated_keys;
 };
 
+// HELPER: Compare two states
 bool matrix_state_equals(matrix_state_t const *const matrix_a, matrix_state_t const *const matrix_b) {
   return (memcmp(matrix_a->state, matrix_b->state, sizeof(matrix_a->state)) == 0);
 }
--- firmware-v2/src/core/matrix/matrix_split.c
+++ firmware-v2/src/core/matrix/matrix_split.c
@@ -11,14 +11,14 @@
 static const uint8_t row_pins[TOTAL_ROWS] = {28, 11, 10, 9, 2, 3, 13, 15}; // GPIO pins for rows
 static const uint8_t col_pins[TOTAL_COLS] = {13, 14, 15, 26, 27, 12, 29, 4, 7, 8, 9, 10, 11, 12}; // GPIO pins for columns
 
+// INIT: Split implementation
 void matrix_init_split(matrix_metadata_t *const metadata) {
-  matrix_split_set_metadata(metadata); // Set the metadata for split matrix
+  matrix_split_set_metadata(metadata);
   for (uint8_t pin_index = 0; pin_index < TOTAL_ROWS_HALF; ++pin_index) {
     gpio_init(metadata->row_pins_half[pin_index]);
     gpio_set_dir(metadata->row_pins_half[pin_index], GPIO_OUT);
   }
 
-  // Init the column pins; set them to input
   for (uint8_t pin_index = 0; pin_index < TOTAL_COLS_HALF; ++pin_index) {
     gpio_init(metadata->col_pins_half[pin_index]);
     gpio_set_dir(metadata->col_pins_half[pin_index], GPIO_IN);
@@ -26,22 +26,21 @@ void matrix_init_split(matrix_metadata_t *const metadata) {
   }
 };
 
+// INIT: set the metadata for the half
 void matrix_split_set_metadata(matrix_metadata_t *const metadata) {
-  // Initialize the metadata for split matrix
-  matrix_initialize_metadata(metadata); // Clear the metadata struct
-  matrix_detect_half(metadata); // Detect which half of the keyboard this is
-  if (metadata->half == RIGHT_HALF) { // Right half
+  matrix_initialize_metadata(metadata);
+  matrix_detect_half(metadata);
+  if (metadata->half == RIGHT_HALF) {
     memcpy(metadata->row_pins_half, &row_pins[TOTAL_ROWS_HALF], sizeof(metadata->row_pins_half));
     memcpy(metadata->col_pins_half, &col_pins[TOTAL_COLS_HALF], sizeof(metadata->col_pins_half));
 
-  } else { // Left half
+  } else {
     memcpy(metadata->row_pins_half, row_pins, sizeof(metadata->row_pins_half));
     memcpy(metadata->col_pins_half, col_pins, sizeof(metadata->col_pins_half));
   }
 }
 
 // TODO: Make this work without usb detection
-// Also, add a initialize function for the struct
 void matrix_detect_half(matrix_metadata_t *const metadata) {
   metadata->half = RIGHT_HALF;
   // if (tud_connected()) {
@@ -51,28 +50,29 @@ void matrix_detect_half(matrix_metadata_t *const metadata) {
   // }
 }
 
+// CORE: Read function for split backend
 void matrix_read_split(matrix_state_t *const state, matrix_metadata_t *const metadata) {
-  // Row doesn't matter, but setting column here instead of in the loop is ideal
   uint8_t row;
   uint8_t col;
   uint8_t destination_index = 0;
+
   // Perform a scan of the matrix
   // We will pull row pins high, then read 
   for (row = 0; row < TOTAL_ROWS_HALF; ++row) {
     gpio_put(metadata->row_pins_half[row], 1);
-    sleep_us(1); // Slight delay to increase accuracy
+    sleep_us(1);
     
     // Read every column pin to check for button presses
     for (col = 0; col < TOTAL_COLS_HALF; ++col) {
       state->state[destination_index++] = gpio_get(metadata->col_pins_half[col]);
     }
+
     // Stop scanning the row
     gpio_put(metadata->row_pins_half[row], 0);
   }
 };
 
+// HELPER: Add one matrix to the other
 void matrix_concatenate(matrix_state_t *const state, const matrix_state_t *right_state) {
-  // dest has room for 2 * HALF_SIZE
-  // Append right_state[0..HALF_SIZE-1] to state[HALF_SIZE..(2*HALF_SIZE)-1]
   memcpy(&state->state[MATRIX_HALF_SIZE], right_state->state, MATRIX_HALF_SIZE);
 }
